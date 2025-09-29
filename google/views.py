9c62c8b14f9a3e89c8cdb2a2c94b35fdd2d22a7a
priyapandey854 <priyadhanaval@gmail.com>
2025-09-29 20:41:01 +0530

    "Google"

--- google/views.py
+++ google/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
+def action(request):
+    return render(request, 'base.html')
 # Create your views here.
